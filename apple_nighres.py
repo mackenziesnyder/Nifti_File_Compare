49d0067f2dc26ea3359c9af026df79788943cbbf
Mackenzie Snyder <masnyder2@hotmail.com>
2025-01-20 10:09:19 -0500

    fix spacing

--- apple_nighres.py
+++ apple_nighres.py
@@ -2,33 +2,25 @@ import nighres
 from shutil import copyfile
 from './prep_for_nighres.py' import prep_for_nighres
 
-
 def apply_nighres(innerbin, outerbin, output_dir):
 
-
    nighres_args = {"save_data": True, "output_dir": output_dir, "overwrite": True}
-
 
    ## convert binarized edges to levelset surfaces
    levelset_inner = nighres.surface.probability_to_levelset(innerbin, **nighres_args)
    levelset_outer = nighres.surface.probability_to_levelset(outerbin, **nighres_args)
 
-
    print("binarized files to levelsets complete")
-
 
    ## get isovolume solution
    isovolume = nighres.laminar.volumetric_layering(
        levelset_inner["result"], levelset_outer["result"], **nighres_args
    )
 
-
    print("levelset to isovolume complete")
-
 
    ## copy to output (volumetric_layering produces multiple files so we need to specify the right one)
    copyfile(isovolume["depth"], output_dir)
-
 
 if __name__ == '__main__':
    input_nii = './laynii_test_data/sc_rim.nii.gz'
