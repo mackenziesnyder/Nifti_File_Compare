50ef130850dcbdd751bf0249e0c2ddd10b6ffe99
mackenziesnyder <masnyder2@hotmail.com>
2025-01-29 16:52:13 -0500

    flattened arrays for computation

--- compute_mse.py
+++ compute_mse.py
@@ -12,15 +12,17 @@ def compute_mse(laynii_file_path, master_file_path):
     pred_laynii = read_laynii_img.get_fdata()
     truth_master = read_master_img.get_fdata()
 
-    mse = mean_squared_error(truth_master, pred_laynii)
+    print("got to here")
+    # flatten arrays for mean_squared_error function
+    mse = mean_squared_error(truth_master.flatten(), pred_laynii.flatten())
     
     return mse
 
 if __name__ == "__main__":
     
     # File paths to LayNii file and master file (edit to correct location)
-    laynii_file_path = "laynii_test_data/sub-01_dir-IO_hemi-L_space-corobl_label-hipp_desc-equivol_coords.nii.gz"
-    master_file_path = "master_test_data/sub-01_dir-IO_hemi-L_space-corobl_label-hipp_desc-equivol_coords.nii.gz"
+    laynii_file_path = "laynii_test_data/sub-01_dir-IO_hemi-R_space-corobl_label-hipp_desc-equidist_coords.nii.gz"
+    master_file_path = "master_test_data/sub-01_dir-IO_hemi-R_space-corobl_label-hipp_desc-equivol_coords.nii.gz"
 
     try:
         # Calculate mse
